0c08a73be061aa55a3896386e1f0dff45356ad88
Serhii <malgothsm@gmail.com>
2021-07-15 16:30:59 +0300

    Trying to catch

--- Information Security Projects/Secure Real Time Multiplayer Game/public/Collectible.mjs
+++ Information Security Projects/Secure Real Time Multiplayer Game/public/Collectible.mjs
@@ -1,8 +1,16 @@
+
 class Collectible {
   constructor({x, y, value, id}) {
-
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.id = id;
+    this.radius = 20
   }
-
+  
+  draw(context,img) {
+    context.drawImage(img, this.x-this.radius, this.y-this.radius, 2*this.radius, 2*this.radius);
+  }
 }
 
 /*
@@ -11,6 +19,8 @@ class Collectible {
 */
 try {
   module.exports = Collectible;
-} catch(e) {}
+} catch(e) {
+
+}
 
 export default Collectible;
