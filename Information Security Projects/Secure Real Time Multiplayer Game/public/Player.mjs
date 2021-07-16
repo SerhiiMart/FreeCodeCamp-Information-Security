25ef1d3181d4f8c05fac32fa49dab0e8c68368a9
Serhii <malgothsm@gmail.com>
2021-07-16 16:53:58 +0300

    Redone everything. Still working on

--- Information Security Projects/Secure Real Time Multiplayer Game/public/Player.mjs
+++ Information Security Projects/Secure Real Time Multiplayer Game/public/Player.mjs
@@ -1,4 +1,18 @@
-import { dimension } from './dimension.mjs';
+const canvasWidth = 640;
+const canvasHeight = 480;
+const border = 10;
+const title = 50; 
+
+const dimension = {
+  canvasWidth: canvasWidth,
+  canvasHeight: canvasHeight,
+  arenaSizeX: canvasWidth - 2 * border,
+  arenaSizeY: canvasHeight - 2 * border - title,
+  minX: border,
+  minY: border + title,
+  maxX: canvasWidth - border,
+  maxY: canvasHeight - border,
+}
 
 class Player {
   constructor({x, y, score, id, radius = 30}) {
